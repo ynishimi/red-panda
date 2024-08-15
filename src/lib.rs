817aad7ae6c74fc7f1ed3ff5142dfe2ea7d236b5
Yuki Nishimi <nishimi.yuki.37z@st.kyoto-u.ac.jp>
2024-08-15 20:37:21 +0900

    save account

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 // use std::error::Error;
-// use std::fs;
+use std::str;
+use std::fs;
 use anyhow::Result;
 use dialoguer::{Input, Password};
 // use security_framework::passwords::{self, get_generic_password};
@@ -8,6 +9,8 @@ use dialoguer::{Input, Password};
 // type MyResult<T> = Result<T, Box<dyn Error>>;
 
 // const SERVICE: &str = "com.apple.network.eap.user.item.wlan.ssid.KUINS-Air";
+// TODO: change path to ~/.config
+const CONFIG_FILE_PATH: &str = "config.yml";
 
 #[derive(Debug)]
 pub struct Credential {
@@ -16,7 +19,17 @@ pub struct Credential {
 }
 
 pub fn login() -> Result<Credential> {
-    let account = get_account()?;
+    let account;
+    let file_result = fs::read(CONFIG_FILE_PATH);
+    match file_result {
+        Ok(file_value) => {
+            account = String::from_utf8(file_value)?;
+            println!("Log in as: {}", account);
+        }
+        Err(_) => {
+            account = get_account()?;
+        }
+    }
     let password = get_password()?;
     Ok(Credential {
         account: account,
@@ -28,7 +41,8 @@ fn get_account() -> Result<String> {
     let account: String = Input::new()
         .with_prompt("Enter your ECS-ID")
         .interact_text()?;
-    
+
+    fs::write(CONFIG_FILE_PATH, &account)?;
     Ok(account)
 }
 

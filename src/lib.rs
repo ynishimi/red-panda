061ede0eda03e996c63d6a4914ca40d6042bbec4
Yuki Nishimi <nishimi.yuki.37z@st.kyoto-u.ac.jp>
2024-08-22 16:43:16 +0900

    login: error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,34 @@
 use std::str;
 use std::fs;
+use anyhow::Error;
 use anyhow::{Result, Context};
 use dialoguer::{Input, Password};
 use reqwest::Client;
 use scraper::{Html, Selector};
-// use serde::{Serialize, Deserialize};
-use serde_urlencoded;
-// use security_framework::passwords::{self, get_generic_password};
-// use clap::Parser;
+use serde::Deserialize;
 
-// type MyResult<T> = Result<T, Box<dyn Error>>;
-
-// const SERVICE: &str = "com.apple.network.eap.user.item.wlan.ssid.KUINS-Air";
-// TODO: change path to ~/.config
 const CONFIG_FILE_PATH: &str = "config.yml";
 
 const LOGIN_URL: &str = "https://panda.ecs.kyoto-u.ac.jp/cas/login?service=https%3A%2F%2Fpanda.ecs.kyoto-u.ac.jp%2Fsakai-login-tool%2Fcontainer";
-// const BASE_URL: &str =  "https://panda.ecs.kyoto-u.ac.jp/direct";
+// const LOGIN_URL: &str = "https://panda.ecs.kyoto-u.ac.jp/cas/login";
+const BASE_URL: &str =  "https://panda.ecs.kyoto-u.ac.jp/direct";
+
 #[derive(Debug)]
 pub struct Credential {
     account: String,
     password: String,
-    // login_token: String,
 }
 
-// #[derive(Debug, serde::Serialize, serde::Deserialize)]
-// struct RequestBody {
-//     username: String,
-//     password: String,
-//     lt: String,
-//     execution: String,
-//     _eventId: String,
-//     submit: String,
-// }
+#[derive(Debug, Deserialize)]
+struct SessionResponse {
+    session_collection: Vec<Session>,
+}
+#[derive(Debug, Deserialize)]
+struct Session {
+    #[serde(rename = "userEid")]
+    user_eid: Option<String>,
+}
+
 
 pub fn get_credential() -> Result<Credential> {
     let account;
@@ -72,7 +68,7 @@ fn get_password() -> Result<String> {
 
 // get login token
 pub async fn get_login_token(client: &Client) -> Result<String> {
-    // let url = "https://panda.ecs.kyoto-u.ac.jp/portal/login";
+
 
     // let client = Client::new();
     let response = client
@@ -89,24 +85,14 @@ pub async fn get_login_token(client: &Client) -> Result<String> {
         .context("Couldn't get login token");
     match lt_parse {
         Ok(lt) => {
-            // println!("{}", lt);
             Ok(lt.to_string())
         },
         Err(e) => Err(e),
     }
 }
 
-pub async fn login(client: &Client, credential: Credential) -> Result<()> {
-    println!("{:?}", credential);
+pub async fn login(client: &Client, credential: &Credential) -> Result<(String)> {
     let login_token = get_login_token(&client).await?;
-    // let request_body = RequestBody {
-    //     username: credential.account,
-    //     password: credential.password,
-    //     lt: login_token,
-    //     execution: String::from("e1s1"),
-    //     _eventId: String::from("submit"),
-    //     submit: String::from("ログイン"),
-    // };
     let params = [
         ("username", credential.account.as_str()),
         ("password", credential.password.as_str()),
@@ -115,25 +101,27 @@ pub async fn login(client: &Client, credential: Credential) -> Result<()> {
         ("_eventId", "submit"),
         ("submit", "ログイン"),
     ];
-    // let request_encoded = serde_urlencoded::to_string(&request_body)?;
-    // println!("{:?}", request_encoded);
     let res_login = client
         .post(LOGIN_URL)
-        .header("Content-Type", "application/x-www-form-urlencoded")
         .form(&params)
         .send()
-        .await?
-        .text()
         .await?;
+    // println!("{:?}", res_login);
 
-    // check if the login succeeded
-    let res = client
-        .get("https://panda.ecs.kyoto-u.ac.jp/direct/session.json")
+    // // check if the login succeeded
+    let res_session: SessionResponse = client
+        // .get(BASE_URL.to_owned() + "/session.json")
+        .get(format!("{}/session.json", BASE_URL))
         .send()
         .await?
-        .text()
+        .json()
         .await?;
-    println!("{}", res);
-    
-    Ok(())
+    let current_session = res_session.session_collection
+        .get(0)
+        .context("No sessions acquired")?
+        .user_eid
+        .clone()
+        .context("Login failed")?;
+    Ok((current_session))
+    // Ok(current_session)
 }
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,10 @@ async fn main() -> Result<()> {
     // let client = Client::new();
     let client = Client::builder().cookie_store(true).build()?;
     let credential = get_credential()?;
-    login(&client, credential).await?;
+    match login(&client, &credential).await {
+    // if let Ok(user) = login(&client, &credential).await {
+        Ok(user) => println!("User: {}", user),
+        Err(e) => eprintln!("{}", e),
+    }
     Ok(())
 }
